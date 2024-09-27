31a8d688bf7b9efe3d66e0e04813aad640c8a3a0
bezalabernaya <nastia.kochikian@mail.ru>
2024-09-27 19:02:38 +0300

    3_maximum_number_of_words solved

--- 1_strings/3_maximum_number_of_words/main.py
+++ 1_strings/3_maximum_number_of_words/main.py
@@ -11,3 +11,12 @@ pytest ./3_maximum_number_of_words/test.py
 
 def get_max_number_of_words_from_sentences(sentences: list[str]) -> bool:
     """Пишите ваш код здесь."""
+    max_num = 0
+    words = 0
+    for i in sentences:
+        for j in i.split():
+            words += 1
+        if words > max_num:
+            max_num = words
+        words = 0
+    return max_num
