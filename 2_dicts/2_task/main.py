f174f1af909487d009b068eff12985e3cc202fe9
bezalabernaya <nastia.kochikian@mail.ru>
2024-10-01 20:40:56 +0300

    2_task solved

--- 2_dicts/2_task/main.py
+++ 2_dicts/2_task/main.py
@@ -1,3 +1,4 @@
+import re
 def top_10_most_common_words(text: str) -> dict[str, int]:
     """Функция возвращает топ 10 слов, встречающихся в тексте.
 
@@ -8,4 +9,10 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
         словарь типа {слово: количество вхождений}
     """
     most_common = {}
-    return most_common
+    match_list = re.findall(r'\b\w{3,}\b', text.lower())
+    for i in sorted(match_list):
+        if i in most_common:
+            most_common[i] += 1
+        else:
+            most_common[i] = 1
+    return dict(sorted(most_common.items(), key=lambda x: x[1], reverse=True)[0:10])
