ca9e5f1bb3a1600f27359d404cac1eae4bef9b09
bezalabernaya <nastia.kochikian@mail.ru>
2024-10-06 18:35:21 +0300

    1_phone_text_code solved

--- 3_functions/1_phone_text_code/decode/decode.py
+++ 3_functions/1_phone_text_code/decode/decode.py
@@ -1,4 +1,26 @@
 def decode_numbers(numbers: str) -> str | None:
-    """Пишите ваш код здесь."""
+    a = ord('а')
+
+    alphabet = '.,?!:;'
+    alphabet += ''.join([chr(i) for i in range(a, a + 32)])
+
+    d = {'0': ' '}
+
+    counter = 0
+    num = 1
+    for t in alphabet:
+        counter += 1
+        d[str(num) * counter] = t
+        if num == 1 and counter == 6 or num != 1 and counter == 4:
+            counter = 0
+            num += 1
+            continue
+
     result = ""
+    for i in numbers.split():
+        if i in d:
+            result += d.get(i)
+        else:
+            return None
+
     return result
--- 3_functions/1_phone_text_code/encode/encode.py
+++ 3_functions/1_phone_text_code/encode/encode.py
@@ -1,4 +1,29 @@
 def encode_text(text: str) -> str | None:
     """Пишите ваш код здесь."""
+
+    a = ord('а')
+
+    alphabet = '.,?!:;'
+    alphabet += ''.join([chr(i) for i in range(a, a + 32)])
+
+    d = {' ': '0'}
+
+    counter = 0
+    num = 1
+    for t in alphabet:
+        counter += 1
+        d[t] = str(num) * counter
+        if num == 1 and counter == 6 or num != 1 and counter == 4:
+            counter = 0
+            num += 1
+            continue
+
     result = ""
-    return result
+    for i in text:
+        if i in d:
+            result += d.get(i)+' '
+        else:
+            return None
+
+    return result[:-1]
+
