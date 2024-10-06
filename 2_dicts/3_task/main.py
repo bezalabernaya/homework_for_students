463a5150cc595d4629a84fa3bdb4af01b28a320b
bezalabernaya <nastia.kochikian@mail.ru>
2024-10-06 18:32:16 +0300

    3_task solved

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def format_phone(phone_number: str) -> dict[str, int]:
     """Функция возвращает отформатированный телефон.
 
@@ -7,6 +10,11 @@ def format_phone(phone_number: str) -> dict[str, int]:
     Returns:
         отформатированный телефон
     """
-    formatted_phone_number = ""
 
-    return formatted_phone_number
+    num = ''.join(re.findall(r'\d+', phone_number))
+
+    if len(num) < 10:
+        return phone_number
+    else:
+        formatted_phone_number = f"8 ({num[-10:-7]}) {num[-7:-4]}-{num[-4]}{num[-3]}-{num[-2]}{num[-1]}"
+        return formatted_phone_number
