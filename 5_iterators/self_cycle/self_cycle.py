ed6fefe8f6b19ae2e5d20c6c0df71449c563c8c7
bezalabernaya <nastia.kochikian@mail.ru>
2024-12-01 18:01:01 +0300

    self_cycle solved

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -9,16 +9,29 @@ def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
         for i in obj:
             yield i
 
+
 class Cycle:
     def __init__(self, obj: Iterable[T]):
         """Реализуйте класс"""
         self.obj = obj
+        self._iter_idx = 0
 
     def __iter__(self):
-        while self.obj:
-            for i in self.obj:
-                yield i
+        return self
 
     def __next__(self):
-        return self
+        if self._iter_idx >= len(self.obj):
+            self._iter_idx = 0
+        try:
+            res = list(self.obj.keys())[self._iter_idx]
+        except AttributeError:
+            try:
+                res = self.obj[self._iter_idx]
+            except TypeError:
+                res = list(self.obj)[self._iter_idx]
+        finally:
+            self._iter_idx += 1
+        return res
+
+
 
