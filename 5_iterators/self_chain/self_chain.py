511c40a02fdabe4f32f3cdd6aebd29e6d3db141a
bezalabernaya <nastia.kochikian@mail.ru>
2024-12-01 18:20:14 +0300

    self_chain solved

--- 5_iterators/self_chain/self_chain.py
+++ 5_iterators/self_chain/self_chain.py
@@ -22,14 +22,16 @@ class Chain:
 
     def __next__(self):
         if self._iter_idx1 < len(self.iterables):
-            if self._iter_idx2 < len(self.iterables[self._iter_idx1]):
-                self._iter_idx2 += 1
-            else:
-                self._iter_idx2 = 1
+            if self._iter_idx2 >= len(self.iterables[self._iter_idx1]):
+                self._iter_idx2 = 0
                 self._iter_idx1 += 1
-        try:
-            return self.iterables[self._iter_idx1][self._iter_idx2 - 1]
-        except Exception:
+            try:
+                res = list(self.iterables[self._iter_idx1].keys())[self._iter_idx2]
+            except AttributeError:
+                res = self.iterables[self._iter_idx1][self._iter_idx2]
+            finally:
+                self._iter_idx2 += 1
+            return res
+        else:
             raise StopIteration
-
 
