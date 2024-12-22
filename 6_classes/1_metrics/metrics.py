bfda2163d8eec5cd3c18736abe94ef757810f0b3
bezalabernaya <nastia.kochikian@mail.ru>
2024-12-22 19:54:25 +0300

    solved v2

--- 6_classes/1_metrics/metrics.py
+++ 6_classes/1_metrics/metrics.py
@@ -1,63 +1,70 @@
 import datetime
-from dateutil import tz
+import csv
 
 
 class Statsd:
-    def __init__(self, path, buffer_limit, sep, start):
+    def __init__(self, writer, buffer_limit):
         """Реализуйте класс"""
-        self.path = path
         self.buffer_limit = buffer_limit
-        self.sep = sep
         self._buffer = []
-        self.writer = Writer()
-        self.writer.check_header(filepath=self.path, start=start)
+        self.writer = writer
+        self.writer.check_header()
 
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.writer.write_metrics(filepath=self.path, b=self._buffer)
+        self.writer.write_metrics(self._buffer)
         return False
 
     def incr(self, name: str):
-        _data = datetime.datetime.now(tz=tz.tzutc()).strftime("%Y-%m-%dT%H:%M:%S%z")
-        self._buffer.append(f"{_data}{self.sep}{name}{self.sep}1")
-        if len(self._buffer) == self.buffer_limit:
-            self.writer.write_metrics(filepath=self.path, b=self._buffer)
+        date = datetime.datetime.now(datetime.UTC)
+        value = 1
+        self._buffer.append([date.strftime("%Y-%m-%dT%H:%M:%S%z"), name, str(value)])
+        if len(self._buffer) >= self.buffer_limit:
+            self.writer.write_metrics(self._buffer)
             self._buffer.clear()
 
     def decr(self, name: str):
-        _data = datetime.datetime.now(tz=tz.tzutc()).strftime("%Y-%m-%dT%H:%M:%S%z")
-        self._buffer.append(f"{_data}{self.sep}{name}{self.sep}-1")
-        if len(self._buffer) == self.buffer_limit:
-            self.writer.write_metrics(filepath=self.path, b=self._buffer)
+        date = datetime.datetime.now(datetime.UTC)
+        value = -1
+        self._buffer.append([date.strftime("%Y-%m-%dT%H:%M:%S%z"), name, str(value)])
+        if len(self._buffer) >= self.buffer_limit:
+            self.writer.write_metrics(self._buffer)
             self._buffer.clear()
 
 
 class Writer:
+    def __init__(self, path):
+        self._path = path
+        self._extension = path[-3::]
 
-    def check_header(self, start, filepath):
-        try:
-            file = open(filepath, "r")
-            line = file.readlines()
-            if line != start:
-                raise
-        except Exception:
-            file = open(filepath, "w")
-            file.write(start)
-        finally:
-            file.close()
+    def check_header(self):
+        if self._extension == 'csv':
+            with open(self._path, "w") as csvfile:
+                writer = csv.writer(csvfile, delimiter=";", lineterminator="\r")
+                writer.writerow(["date", "metric", "value"])
+        elif self._extension == 'txt':
+            with open(self._path, "w") as file:
+                pass
 
-    def write_metrics(self, filepath: str, b, ):
-        with open(filepath, "a") as file:
-            for line in b:
-                file.write(line + '\n')
+    def write_metrics(self, buffer):
+        if self._extension == 'txt':
+            with open(self._path, "a") as file:
+                for line in buffer:
+                    file.write(f'{line[0]} {line[1]} {line[2]}\n')
+        elif self._extension == 'csv':
+            with open(self._path, "a") as csvfile:
+                writer = csv.writer(csvfile, delimiter=";", lineterminator="\r")
+                for line in buffer:
+                    writer.writerow(line)
 
 
 def get_txt_statsd(path: str, buffer_limit: int = 10) -> Statsd:
     """Реализуйте инициализацию метрик для текстового файла"""
     if path.endswith('.txt'):
-        return Statsd(path, buffer_limit, sep=' ', start='')
+        writer = Writer(path=path)
+        return Statsd(writer, buffer_limit)
     else:
         raise ValueError
 
@@ -65,10 +72,10 @@ def get_txt_statsd(path: str, buffer_limit: int = 10) -> Statsd:
 def get_csv_statsd(path: str, buffer_limit: int = 10) -> Statsd:
     """Реализуйте инициализацию метрик для csv файла"""
     if path.endswith('.csv'):
-        return Statsd(path, buffer_limit, sep=';', start='date;metric;value\n')
+        writer = Writer(path=path)
+        return Statsd(writer, buffer_limit)
     else:
         raise ValueError
-
 
 
 
